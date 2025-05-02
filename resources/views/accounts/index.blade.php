1d2e664aa720971db363e78cf0abdd26e0f9e639
Tom <tom.gellido@gmail.com>
2025-05-02 20:41:07 +0800

    use extends on account list

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -1,63 +1,64 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
-</head>
-<body>
-<div class="container mx-auto px-4 sm:px-8">
-    <div class="py-8">
-        <div class="flex justify-between">
-            <h2 class="text-2xl font-semibold leading-tight">Accounts</h2>
-        </div>
-        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-            <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
-                <table class="min-w-full leading-normal">
-                    <thead>
-                    <tr>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                            Id
-                        </th>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                            Name
-                        </th>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                            Email
-                        </th>
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                            Avaible Balance
-                        </th>
+@extends('layouts.app')
 
-                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($accounts as $item)
+@section('content')
+    <div class="container mx-auto px-4 sm:px-8">
+        <div class="py-8">
+            <div class="flex justify-between">
+                <h2 class="text-2xl font-semibold leading-tight">Accounts</h2>
+            </div>
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
+                    <table class="min-w-full leading-normal">
+                        <thead>
                         <tr>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                {{ $item->id }}
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                {{ $item->name }}
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                {{ $item->email }}
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                {{ $item->available_balance }}
-                            </td>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Id
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Name
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Email
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Avaible Balance
+                            </th>
 
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                                <a href="{{route('transactions.index', $item->id)}}" class="px-4 py-2 bg-blue-400 rounded-lg mr-3 font-bold">View Transactions</a>
-                            </td>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach($accounts as $item)
+                            <tr>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    {{ $item->id }}
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    {{ $item->name }}
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    {{ $item->email }}
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    {{ $item->available_balance }}
+                                </td>
+
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+                                    <a href="{{route('transactions.index', $item->id)}}" class="px-4 py-2 bg-blue-400 rounded-lg mr-3 font-bold">View Transactions</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div></div>
-</body>
-</html>
+@endsection
+
+
+
+
+
+
+
